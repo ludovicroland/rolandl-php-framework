44d20a24674108fc316fd53c7b0b68aa71450357
ludovicroland <ludovic@smartnsoft.com>
2017-03-16 23:11:08 +0100

    Remove the "^M" character from the sources

--- Library/PDOFactory.class.php
+++ Library/PDOFactory.class.php
@@ -1,1 +1,15 @@
-<?phpnamespace Library;class PDOFactory{  public static function getMysqlConnexion()  {    $db = new \PDO("mysql:host=localhost;dbname=myDB", "login", "password");    $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);    return $db;  }}
+<?php
+namespace Library;
+
+class PDOFactory
+{
+
+  public static function getMysqlConnexion()
+  {
+    $db = new \PDO("mysql:host=localhost;dbname=myDB", "login", "password");
+    $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
+    return $db;
+  }
+
+}
--- Library/URLFormatter.class.php
+++ Library/URLFormatter.class.php
@@ -1,1 +1,24 @@
-<?phpnamespace Library;class URLFormatter{  public static function format($libelle)  {    $libelle = trim($libelle);    $a = "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿŔŕ";    $b = "aaaaaaaceeeeiiiidnoooooouuuuybsaaaaaaaceeeeiiiidnoooooouuuyybyRr";    $libelle = utf8_decode($libelle);    $libelle = strtr($libelle, utf8_decode($a), $b);    $libelle = trim($libelle);    $libelle = preg_replace("#[0-9]#", "", $libelle);    $libelle = trim($libelle);    $libelle = preg_replace("#[^a-zA-Z ]#", "", $libelle);    $libelle = trim($libelle);    $libelle = preg_replace("/\\s+/", "-", $libelle);    return strtolower($libelle);  }}
+<?php
+namespace Library;
+
+class URLFormatter
+{
+
+  public static function format($libelle)
+  {
+    $a = "ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿŔŕ";
+    $b = "aaaaaaaceeeeiiiidnoooooouuuuybsaaaaaaaceeeeiiiidnoooooouuuyybyRr";
+
+    $libelle = utf8_decode($libelle);
+    $libelle = strstr($libelle, utf8_decode($a), $b);
+    $libelle = trim($libelle);
+    $libelle = preg_replace("#[0-9]#", "", $libelle);
+    $libelle = trim($libelle);
+    $libelle = preg_replace("#[^a-zA-Z ]#", "", $libelle);
+    $libelle = trim($libelle);
+    $libelle = preg_replace("/\\s+/", "-", $libelle);
+
+    return strtolower($libelle);
+  }
+
+}
